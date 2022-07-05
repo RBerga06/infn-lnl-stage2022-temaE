8cf9204da179a12c8eea0cdaa75314ee06ca4838
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-07-05 11:23:05 +0200

    Risolta la spaziatura

--- src/stagisti.py
+++ src/stagisti.py
@@ -5,7 +5,7 @@ for i in range(0x16):
 stagisti = ["Rosalinda", "Jacopo", "Giacomo", "Riccardo"]
 risultati = []
 while len(risultati) < 4:
-     stagista = stagisti[g.random_number() % 4]
-     if not stagista in risultati:
-             risultati.append(stagista)
+    stagista = stagisti[g.random_number() % 4]
+    if not stagista in risultati:
+        risultati.append(stagista)
 print(risultati)
