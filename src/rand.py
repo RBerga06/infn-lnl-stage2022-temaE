20264fbb3c7c14102cd419ec07993649c91f7b3c
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-07-09 13:52:42 +0200

    Update rand.py

--- src/rand.py
+++ src/rand.py
@@ -202,12 +202,13 @@ class PLOT(Flag):
 # Specifica cosa mostrare usando le flag appena definite.
 #   Per combinarle, usare l'operatore «|»
 #   In pratica, basta commentare qua sotto le righe corrispondenti ad elementi o grafici da *non* mostrare
-TO_PLOT: PLOT = (PLOT.NOTHING
+TO_PLOT: PLOT = \
+    (PLOT.NOTHING
     | PLOT.TIME_DELTAS
     | PLOT.BITS_DISTRIBUTION
     | PLOT.BYTES_DISTRIBUTION
     | PLOT.BYTES_DISTRIBUTION_LOCAL_MEANS
-)
+    )
 
 
 # Funzione per calcolare le medie locali (ciclicamente)
