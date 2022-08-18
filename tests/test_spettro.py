2d2d00c58de6533bcd870f9229f23ff2e6e7471d
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-08-18 12:35:19 +0200

    Create test_spettro.py

---
+++ tests/test_spettro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=import-error
+"""Test `spettro.py`."""
+from __future__ import annotations
+
+from spettro import Event, aree, main as _test
+
+
+def test_ui():
+    """Test `main(...)`"""
+    _test()
