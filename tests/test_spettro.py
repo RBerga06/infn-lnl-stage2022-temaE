0ea143e0fde5cc3654f91da134364fcbf372f9d7
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-08-18 12:49:59 +0200

    Update test_spettro.py

--- tests/test_spettro.py
+++ tests/test_spettro.py
@@ -3,10 +3,15 @@
 # pylint: disable=import-error
 """Test `spettro.py`."""
 from __future__ import annotations
+from pathlib import Path
+import sys
 
 from spettro import main as _test
 
 
 def test_ui():
     """Test `main(...)`"""
+    argv = sys.argv.copy()
+    sys.argv.insert(1, str((Path(__file__).parent.parent/"src"/"fondo.root").resolve()))
     _test()
+    sys.argv = argv.copy()
