b9ed3aeff97a33477d3fc865cc3c00ffed684916
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-08-19 17:21:13 +0200

    Update test_spettro.py

--- tests/test_spettro.py
+++ tests/test_spettro.py
@@ -5,12 +5,11 @@
 from __future__ import annotations
 import sys
 from pathlib import Path
-import matplotlib.pyplot as plt
 from conftest import sys_argv, mpl_test
 from spettro import main as _test
 
 
-@mpl_test("background_energy_spectrum")
+@mpl_test(filename="spettro.png")
 def test_ui():
     """Test `main(...)`"""
     with sys_argv([sys.argv[0], str((Path(__file__).parent.parent/"src"/"fondo.root").resolve()), *sys.argv[1:]]):
